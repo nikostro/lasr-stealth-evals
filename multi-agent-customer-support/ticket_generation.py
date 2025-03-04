b83e32bf826c6b25e25e60fcd3a0eed3705e464b
Nikita Ostrovsky <nik.ostro@gmail.com>
2025-03-04 12:18:29 +0000

    CHORE: create protocol and ticket_generation scripts

---
+++ multi-agent-customer-support/ticket_generation.py
@@ -0,0 +1,11 @@
+# randomly sample from protocol
+
+# query ticket generator to request ticket
+
+# generate ticket description
+
+# put into {task_type, effort_points, description}
+
+# save ticket to table
+
+# append to list of tickets
