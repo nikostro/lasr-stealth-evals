ba998b33b3052fa49e220b84223a47a8a724b393
Nikita Ostrovsky <nik.ostro@gmail.com>
2025-04-10 12:41:02 +0100

    FEAT: use json

--- src/lasr_stealth_evals/library/util.py
+++ src/lasr_stealth_evals/library/util.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 DATASET_PATH = Path(__file__).parent.parent.parent.parent / "data" / "dataset.jsonl"
 
+
 def load_dataset(path: str = str(DATASET_PATH)) -> list[dict[str, Any]]:
     f = open(path, "r")
-    dataset = [json.loads(line) for line in list(f)]
+    dataset = json.load(f)
     f.close()
     return dataset
+
 
 print(load_dataset())
